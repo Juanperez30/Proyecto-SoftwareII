d5187eb478e64c19c03efcf643ea7b498c2cd90a
Juan Perez <juanpabloperezleal@gmail.com>
2022-03-06 21:09:37 -0500

    Pagina con template

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from multiprocessing import context
+from django.shortcuts import render
+from .models import Item
+
+def item_list(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, "home-page.html", context)
