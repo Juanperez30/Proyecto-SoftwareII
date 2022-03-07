77725bbec5b952b74f762b54aea8af88121d38ae
Juan Perez <juanpabloperezleal@gmail.com>
2022-03-07 00:53:12 -0500

    Proyecto final
    Falta arreglar para que pueda filtrar por articulo

--- core/views.py
+++
@@ -1,9 +0,0 @@
-from multiprocessing import context
-from django.shortcuts import render
-from .models import Item
-
-def item_list(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, "home-page.html", context)
